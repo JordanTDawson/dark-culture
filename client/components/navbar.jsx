64df19a2f0a62ba5b82d2fd240124226578595ed
Jordan Dawson <jtdawson@icloud.com>
2023-02-25 23:12:12 +0000

    transferring to home machine.

---
+++ client/components/navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon" />
+      </button>
+      <a className="navbar-brand" href="#">Navbar</a>
+
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Link</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled">Disabled</a>
+          </li>
+        </ul>
+        <form className="form-inline my-2 my-lg-0">
+          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </input>
+        </form>
+      </div>
+    </nav>
+  );
+}
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import HelloWorld from '../components/hello-world';
+import NavBar from '../components/navbar';
 
 export default function Home(props) {
   return (
     <div>
+      <NavBar />
       <HelloWorld />
     </div>
   );
