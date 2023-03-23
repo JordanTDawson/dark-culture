381110ce05f24c2d2ac93378a72043f1afbe7246
JordanTDawson <jtdawson@icloud.com>
2023-03-23 19:57:56 +0000

    cleaning up home page. Saving for safety.

--- client/catalog-components/products.jsx
+++ client/catalog-components/products.jsx
@@ -24,7 +24,7 @@ export default function Products({ catalog }) {
             </Card>
           </a>
         </Col>
-      ))};
+      ))}
     </Row>
   );
 }
--- client/home-components/hero-image.jsx
+++ client/home-components/hero-image.jsx
@@ -9,13 +9,13 @@ const cardText = {
   fontSize: '30px'
 };
 
-export default function HeroImage(props) {
+export default function HeroImage() {
   return (
-    <Card className="bg-dark text-white rounded-0">
-      <Card.Img style={{ height: 400 }} src="https://www.npd.com/wp-content/uploads/2021/05/fashion-banner-1440x480.jpg" />
+    <Card className="bg-light text-dark">
+      <Card.Img className="rounded-0" style={{ height: 500 }} src="/images/hero-image.png" />
       <Card.ImgOverlay>
-        <Card.Title style={cardTitle} className="text-center mt-5">Welcome to our Shop!</Card.Title>
-        <Card.Text style={cardText} className="text-center mt-4">
+        <Card.Title style={cardTitle} className="text-center mt-2 pt-2">Welcome to our Shop!</Card.Title>
+        <Card.Text style={cardText} className="text-center mt-5 pt-5">
           We have some of the finest clothing around! Check out our shop and you
           might find something that you would want to ship to your front doorstep!
         </Card.Text>
