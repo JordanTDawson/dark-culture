5be1f9324acaeedbde8fec160c889b597ca119b1
JordanTDawson <jtdawson@icloud.com>
2023-05-03 18:56:24 +0000

    Changed Bootstrap for three column image.

--- client/home-components/three-column-image.jsx
+++ client/home-components/three-column-image.jsx
@@ -3,35 +3,28 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
-import Stack from 'react-bootstrap/Stack';
 
 export default function ThreeImageCol() {
   return (
     <Container fluid>
-      <Row className="text-center my-4">
-        <Col sm={4}>
-          <Stack className="bg-secondary rounded" direction="horizontal" gap={3}>
+      <Row xs={1} sm={2} md={3} className="text-center my-4">
+        <Col>
+          <a className="text-decoration-none text-black" href="#catalog">
             <Image className="rounded" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTODucBZAM1Q6RT89Eny1tuAUkF_6Xa5OL4nQ&usqp=CAU" />
-            <a className="text-decoration-none text-black" href="#catalog">
-              <h1>Men&apos;s Shirts</h1>
-            </a>
-          </Stack>
+            <h1>Men&apos;s Shirts</h1>
+          </a>
         </Col>
-        <Col sm={4}>
-          <Stack className="bg-secondary rounded" direction="horizontal" gap={3} >
+        <Col>
+          <a className="text-decoration-none text-black" href="#catalog">
             <Image className="rounded" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaSEZYiL4voHUgvWmseyo1XIHlj_RjHPerki507aXPOBhf6iXis6VSggrOiUATt6Ym2Uc&usqp=CAU" />
-            <a className="text-decoration-none text-black" href="#catalog">
-              <h1>Men&apos;s Pants</h1>
-            </a>
-          </Stack>
+            <h1>Men&apos;s Pants</h1>
+          </a>
         </Col>
-        <Col sm={4}>
-          <Stack className="bg-secondary rounded" direction="horizontal" gap={3} >
+        <Col>
+          <a className="text-decoration-none text-black" href="#catalog" >
             <Image className="rounded" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaSEZYiL4voHUgvWmseyo1XIHlj_RjHPerki507aXPOBhf6iXis6VSggrOiUATt6Ym2Uc&usqp=CAU" />
-            <a className="text-decoration-none text-black" href="#catalog">
-              <h1>Men&apos;s Jackets</h1>
-            </a>
-          </Stack>
+            <h1>Men&apos;s Jackets</h1>
+          </a>
         </Col>
       </Row>
     </Container>
