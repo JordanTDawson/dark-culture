66b63c083587558949b6f7498c7d15ba1ebb2bba
JordanTDawson <jtdawson@icloud.com>
2023-06-12 00:17:38 +0000

    forgot to save the cart file.

--- client/catalog-components/cart-items.jsx
+++ client/catalog-components/cart-items.jsx
@@ -4,60 +4,59 @@ import Button from 'react-bootstrap/Button';
 import Message from './cart-message';
 
 export default function CartItems({ productId, updateTotalPrice }) {
-
-  const [cartItem, setCartItem] = useState();
+  const [cartItem, setCartItem] = useState(null);
   const [message, setMessage] = useState(null);
 
-  function handleItemRemoval(productId) {
-    fetch(`/api/shoppingCatalog/CartItems/${productId}`, {
-      method: 'DELETE',
-      headers: { 'Content-Type': 'application/json' }
-    })
-      .then(res => {
-        if (res.status === 204) {
-          fetch('/api/shoppingCatalog/CartItems')
-            .then(res => res.json())
-            .then(cart => {
-              updateTotalPrice(cart);
-              setMessage('Item removed successfully.');
-              setTimeout(() => {
-                setMessage(null);
-              }, 1000);
-            })
-            .catch(err => console.error(err));
-        } else {
-          throw new Error('Failed to delete item');
-        }
-      })
-      .catch(err => {
-        console.error(err);
-        setMessage('Failed to delete item.');
-      })
-  }
+  const handleItemRemoval = async (productId) => {
+    try {
+      const res = await fetch(`/api/shoppingCatalog/CartItems/${productId}`, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+      });
+
+      if (res.status === 204) {
+        const cartResponse = await fetch('/api/shoppingCatalog/CartItems');
+        const cart = await cartResponse.json();
+        updateTotalPrice(cart);
+        setMessage('Item removed successfully.');
+        setTimeout(() => {
+          setMessage(null);
+        }, 1000);
+      } else {
+        throw new Error('Failed to delete item');
+      }
+    } catch (error) {
+      console.error(error);
+      setMessage('Failed to delete item.');
+    }
+  };
 
   useEffect(() => {
-    fetch(`/api/shoppingCatalog/Catalog/${productId}`)
-      .then(res => {
+    const fetchCartItem = async () => {
+      try {
+        const res = await fetch(`/api/shoppingCatalog/Catalog/${productId}`);
         if (res.status === 404) {
           throw new Error('Cart not found');
         }
-        return res.json();
-      })
-      .then(cartItem => {
-        setCartItem(cartItem)
-      })
-      .catch(err => console.error(err));
+        const cartItem = await res.json();
+        setCartItem(cartItem);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchCartItem();
   }, [productId]);
 
   return (
     <div>
       {message && <Message text={message} />}
       {cartItem && (
-        <Card className="catalog-item mx-1 my-4" >
+        <Card className="catalog-item mx-1 my-4">
           <Card.Img className="catalog-image" alt={cartItem.itemName} variant="top" src={cartItem.itemImage} />
           <Card.Body className="text-center">
-            <Card.Title className="title-font" >{cartItem.itemName}</Card.Title>
-            <Card.Text className="body-font" >${cartItem.price.toFixed(2)}</Card.Text>
+            <Card.Title className="title-font">{cartItem.itemName}</Card.Title>
+            <Card.Text className="body-font">${cartItem.price.toFixed(2)}</Card.Text>
             <Button variant="danger" onClick={() => handleItemRemoval(cartItem.productId)}>
               <div className="body-font">Remove Item</div>
             </Button>
